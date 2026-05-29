a9b56ad45014b7416c5cad0aaa6d399cce5ba45d
luisvasquezgonzalez <luicho.vasquez44@gmail.com>
2026-05-29 06:25:56 -0400

    Documented all methods in Chips.java using Javadocs

--- src/main/java/com/plurasight/Chips.java
+++ src/main/java/com/plurasight/Chips.java
@@ -2,18 +2,37 @@ package com.plurasight;
 
 import com.plurasight.Enums.ChipsType;
 
-public class Chips extends Item{
+/**
+ * This class represents a chips item in the order.
+ * A chips item has a chips type and a fixed price.
+ */
+public class Chips extends Item {
     private ChipsType chipsType;
 
-    public Chips(ChipsType chipsType){
+    /**
+     * Creates a Chips object with the selected chips type.
+     *
+     * @param chipsType The type of chips selected by the user.
+     */
+    public Chips(ChipsType chipsType) {
         this.chipsType = chipsType;
     }
 
+    /**
+     * Gets the price of the chips.
+     *
+     * @return The price of the chips.
+     */
     @Override
     public double getValue() {
         return 1.5;
     }
 
+    /**
+     * Gets the full details of the chips for the receipt.
+     *
+     * @return A formatted String with the chips name, price, and type.
+     */
     @Override
     public String getDetails() {
         String chipsString = "Chips";
@@ -22,12 +41,23 @@ public class Chips extends Item{
 
     }
 
+    /**
+     * Gets a short summary of the chips item.
+     * This is used when showing the item in a menu or order list.
+     *
+     * @return A short String with the chips type and price.
+     */
     @Override
     public String getItemHeader() {
-        return "Chips {"+chipsType+", $"+getValue()+"}";
+        return "Chips {" + chipsType + ", $" + getValue() + "}";
     }
 
-    public void setChipsType(ChipsType chipsType){
+    /**
+     * Changes the chips type.
+     *
+     * @param chipsType The new chips type.
+     */
+    public void setChipsType(ChipsType chipsType) {
         this.chipsType = chipsType;
     }
 }
