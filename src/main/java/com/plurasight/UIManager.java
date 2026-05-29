0a7bb770232b89f095774e978070a95fcf4797f3
luisvasquezgonzalez <luicho.vasquez44@gmail.com>
2026-05-29 01:22:41 -0400

    Added the feature to add a name to the order. Now the customer name shows in the receipt and if no name is provided it shows No-Name

--- src/main/java/com/plurasight/UIManager.java
+++ src/main/java/com/plurasight/UIManager.java
@@ -14,8 +14,8 @@ public class UIManager {
         do {
             System.out.println("MAIN MENU");
             System.out.println("""
-                   \t1.New Order
-                   \t2.Exit""");
+                    \t1.New Order
+                    \t2.Exit""");
             try {
                 int userInput = UIComponent.getUserInput(scanner);
                 switch (userInput) {
@@ -29,90 +29,87 @@ public class UIManager {
                         System.out.println("Select an option provided.");
                 }
             } catch (InputMismatchException ime) {
-               System.err.println("Error: Select a numeric value from the options provided");
-               scanner.nextLine();
+                System.err.println("Error: Select a numeric value from the options provided");
+                scanner.nextLine();
             }
         } while (isRunning);
     }
 
 
-    public void newOrder(){
+    public void newOrder() {
 
         newOrder = new Order();
         boolean needsInput = true;
         do {
             System.out.println("ORDER SCREEN");
             System.out.println("""
-                \t1.Add Sandwich
-                \t2.Add Signature Sandwich
-                \t3.Add Drink
-                \t4.Add Chips
-                \t5.Checkout
-                \t0.Cancel Order""");
+                    \t1.Add Sandwich
+                    \t2.Add Signature Sandwich
+                    \t3.Add Drink
+                    \t4.Add Chips
+                    \t5.Add Customer Name to the order
+                    \t6.Checkout
+                    \t0.Cancel Order""");
             try {
                 int userInput = UIComponent.getUserInput(scanner);
-            switch (userInput){
-                case 1:
-                    SandwichUIComponent sandwichUI= new SandwichUIComponent(scanner);
-                    sandwichUI.displayComponent();
-                    newOrder.addItem(sandwichUI.getItem());
-                    break;
-                case 2:
+                switch (userInput) {
+                    case 1:
+                        SandwichUIComponent sandwichUI = new SandwichUIComponent(scanner);
+                        sandwichUI.displayComponent();
+                        newOrder.addItem(sandwichUI.getItem());
+                        break;
+                    case 2:
 
-                    SignatureSadwichUIComponent signatureSadwichUIComponent = new SignatureSadwichUIComponent(scanner);
-                    signatureSadwichUIComponent.displayComponent();
-                    newOrder.addItem(signatureSadwichUIComponent.getSignatureSandwich());
+                        SignatureSadwichUIComponent signatureSadwichUIComponent = new SignatureSadwichUIComponent(scanner);
+                        signatureSadwichUIComponent.displayComponent();
+                        newOrder.addItem(signatureSadwichUIComponent.getSignatureSandwich());
 
-                    break;
-                case 3:
-                    DrinkUIComponent drinkUi = new DrinkUIComponent(scanner);
-                    drinkUi.displayComponent();
-                    newOrder.addItem(drinkUi.getItem());
-                    break;
-                case 4:
-                    ChipsUIComponent chipsUIComponent = new ChipsUIComponent(scanner);
-                    chipsUIComponent.displayComponent();;
-                    newOrder.addItem(chipsUIComponent.getItem());
-                    break;
-                case 5:
-                    if (!(newOrder.isEmpty())) {
-                        CheckoutUIComponent checkoutUIComponent = new CheckoutUIComponent(scanner, newOrder);
-                        checkoutUIComponent.displayComponent();
-                        if (checkoutUIComponent.isOrderCheckedout()) {
-                            newOrder = null;
-                            needsInput = false;
+                        break;
+                    case 3:
+                        DrinkUIComponent drinkUi = new DrinkUIComponent(scanner);
+                        drinkUi.displayComponent();
+                        newOrder.addItem(drinkUi.getItem());
+                        break;
+                    case 4:
+                        ChipsUIComponent chipsUIComponent = new ChipsUIComponent(scanner);
+                        chipsUIComponent.displayComponent();
+                        ;
+                        newOrder.addItem(chipsUIComponent.getItem());
+                        break;
+                    case 5:
+                        addCustomerNameToOrder();
+                        break;
+                    case 6:
+                        if (!(newOrder.isEmpty())) {
+                            CheckoutUIComponent checkoutUIComponent = new CheckoutUIComponent(scanner, newOrder);
+                            checkoutUIComponent.displayComponent();
+                            if (checkoutUIComponent.isOrderCheckedout()) {
+                                newOrder = null;
+                                needsInput = false;
+                            }
+                        } else {
+                            System.out.println("There must be at least ONE item in your order to checkout");
                         }
-                    }else {
-                        System.out.println("There must be at least ONE item in your order to checkout");
-                    }
-                    break;
-                case 0 :
-                    newOrder = null;
-                    needsInput = false;
-                    break;
-                default:
-                    System.out.println("Select an option provided");
-            }
+                        break;
+                    case 0:
+                        newOrder = null;
+                        needsInput = false;
+                        break;
+                    default:
+                        System.out.println("Select an option provided");
+                }
             } catch (InputMismatchException ime) {
                 System.err.println("Error: Select a numeric value from the options provided");
                 scanner.nextLine();
             }
 
-        }while (needsInput);
-
-
-    }
-
-    private void checkout() {
-        System.out.println("CHECKOUT");
-        System.out.println("\n"+"|Item|\t\t\t\t\t\tPrice");
-        System.out.println(newOrder.getOrderDetails());
+        } while (needsInput);
 
     }
 
-
-
-
-
-
+    private void addCustomerNameToOrder() {
+        System.out.println("Enter customer name: ");
+        String customerName = scanner.nextLine();
+        newOrder.setCustomerName(customerName);
+    }
 }
